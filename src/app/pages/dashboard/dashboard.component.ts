8778955f451399ce6f8761fda2e277c6e213da9e
James-Trema <andrdu29@live.fr>
2024-10-23 10:24:46 +0200

    up

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -18,4 +18,14 @@ export class DashboardComponent {
     this.authService.logout(); // Appel à la méthode logout du service d'authentification
     this.router.navigate(['/login']); // Rediriger vers la page de connexion
   }
+
+  // Naviguer vers la page d'ajout d'utilisateur
+  navigateToAddUser() {
+    this.router.navigate(['/add-user']); // Assurez-vous que la route /add-user existe dans app.routes.ts
+  }
+
+  // Naviguer vers la liste des utilisateurs
+  navigateToUserList() {
+    this.router.navigate(['/user-list']); // Assurez-vous que la route /user-list existe dans app.routes.ts
+  }
 }
