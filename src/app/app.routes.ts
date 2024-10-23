17e838faa9a5e477d3b70b91106a2fc3bd23d9c5
James-Trema <andrdu29@live.fr>
2024-10-23 10:04:20 +0200

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { AuthGuard } from './guards/auth.guard'; // Le garde qui protège l'accès aux routes sécurisées
-import { LayoutComponent } from './components/layout/layout.component'; // À définir si nécessaire
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
-    path: '', component: LayoutComponent, canActivate: [AuthGuard], // Protéger la route principale par AuthGuard
-    children: [
-      { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] }, // Protéger également le tableau de bord
-      // Ajouter d'autres enfants si nécessaire, tous sécurisés par AuthGuard
-    ]
+    path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] // Protéger directement cette route
   },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Rediriger vers la page de connexion par défaut
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirection par défaut
   { path: '**', redirectTo: 'login' } // Gérer les routes non reconnues
 ];
