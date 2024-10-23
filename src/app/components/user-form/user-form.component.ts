e8e2d84b0185fd642b7294f080afd5ee4d43a7ef
James-Trema <andrdu29@live.fr>
2024-10-23 15:06:11 +0200

    up

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserForm } from '../../interface/user-form'; // Utilisation de l'interface UserForm
+import { UserService } from '../../services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
-export class UserFormComponent {
 
+export class UserFormComponent {
+  userForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private userService: UserService) {
+    this.userForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      const formData: UserForm = this.userForm.value as UserForm;
+      this.userService.addUser(formData);
+      console.log('Utilisateur ajouté : ', formData);
+    }
+  }
 }
--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../interface/user';
+import { UserService } from '../../services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
-export class UserListComponent {
 
+export class UserListComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.users = this.userService.getUsers();
+  }
+
+  deleteUser(user: User) {
+    this.userService.deleteUser(user);
+    console.log('Utilisateur supprimé : ', user);
+  }
+
+  editUser(user: User) {
+    console.log('Modification utilisateur : ', user);
+  }
+
+  viewDetails(user: User) {
+    console.log('Détails utilisateur : ', user);
+  }
 }
