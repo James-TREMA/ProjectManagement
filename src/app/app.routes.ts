82c29fc775052245d9adf1fca848abc8cb7c5cb2
James-Trema <andrdu29@live.fr>
2024-10-23 10:52:28 +0200

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,17 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { AuthGuard } from './guards/auth.guard';
-import { AddUserComponent } from './components/add-user/add-user.component'; 
+import { UserFormComponent } from './components/user-form/user-form.component'; 
 import { UserListComponent } from './components/user-list/user-list.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
-    path: '', canActivate: [AuthGuard], // Protéger la route principale
+    path: '', canActivate: [AuthGuard], // Protéger la route principale avec AuthGuard
     children: [
-      { path: 'dashboard', component: DashboardComponent }, // Tableau de bord protégé
-      { path: 'add-user', component: AddUserComponent }, // Ajout d'utilisateur protégé
-      { path: 'user-list', component: UserListComponent } // Liste des utilisateurs protégée
+      { path: 'dashboard', component: DashboardComponent }, // Tableau de bord
+      { path: 'add-user', component: UserFormComponent }, // Ajout d'utilisateur avec le formulaire
+      { path: 'user-list', component: UserListComponent } // Liste des utilisateurs
     ]
   },
   { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirection par défaut
